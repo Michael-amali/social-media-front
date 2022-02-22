58192c451c8f5bc371bb2f78442ea64b17bd64d5
Michael-amali <michael.acheampong@amalitech.com>
2022-02-22 12:19:05 +0100

    Add dist folder

---
+++ dist/js/chunk-2d22d746.2aadb56b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],u={name:"About",components:{},data:function(){return{}},methods:{}},c=u,o=e("2877"),r=Object(o["a"])(c,a,s,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.2aadb56b.js.map
