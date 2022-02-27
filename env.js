24248feb7045e39f1e249b6c45294050781e2cc5
Michael-amali <michael.acheampong@amalitech.com>
2022-02-27 10:31:19 +0100

    Remove env.js file from .gitignore

---
+++ env.js
@@ -0,0 +1,7 @@
+const CLOUD_NAME = "cloudfisk";
+const CLOUD_UPLOAD_PRESET = "f6hwsyel";
+
+// const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://social-media-myk.herokuapp.com";
+
+export { CLOUD_NAME, CLOUD_UPLOAD_PRESET, BASE_URL };
